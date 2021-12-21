7e04e51c5b415504d84f5bb5af403fcc326fa52f
Simone Manti <manti998@gmail.com>
2021-12-21 11:20:46 +0100

    Small changes on cup.py for test phase

--- cup.py
+++ cup.py
@@ -10,7 +10,7 @@ from mlprj.feed_forward import Network, Layer
 from mlprj.randomized_nn import *
 from sklearn.model_selection import train_test_split
 
-def cup_build_model(learning_rate, alpha, lambdareg = 0, decay = None):
+def cup_build_model_train(learning_rate, alpha, lambdareg = 0, decay = None):
     nn = Network(10, [ 
         Layer(50, "relu", GaussianInitializer()),
         Layer(20, "sigmoid", GaussianInitializer()),
@@ -21,6 +21,16 @@ def cup_build_model(learning_rate, alpha, lambdareg = 0, decay = None):
     nn.compile(loss=MSE(), regularizer=L2Regularizer(lambdareg), optimizer=StochasticGradientDescent(learning_rate, alpha, decay))
     return nn
 
+def cup_build_model_test(learning_rate, alpha, lambdareg = 0, decay = None):
+    nn = Network(10, [ 
+        Layer(50, "relu", GaussianInitializer()),
+        Layer(20, "sigmoid", GaussianInitializer()),
+        Layer(10, "relu", GaussianInitializer()),
+        Layer(2, "linear", GaussianInitializer())
+      ])
+
+    nn.compile(loss=MEE(), regularizer=L2Regularizer(lambdareg), optimizer=StochasticGradientDescent(learning_rate, alpha, decay))
+    return nn
 
 
 if __name__ == "__main__":
@@ -29,19 +39,18 @@ if __name__ == "__main__":
     cup_train_x, cup_test_x, cup_train_y, cup_test_y = train_test_split(cup_X, cup_y, test_size=0.10, random_state=42)
     
     cup_params = {
-        "learning_rate" : [0.1, 0.2],
-        "alpha" : [0.1, 0.2],
-        "lambdareg" : [0.00001, 0.00005],
-        "decay": [(10, 0.01), (20, 0.01)],
+        "learning_rate" : [0.01, 0.05, 0.1],
+        "alpha" : [0.15, 0.20, 0.25],
+        "lambdareg" : [0.00001, 0.00005, 0.0001],
         "epochs" : 500,
-        "batch_size" : 64,
+        "batch_size" : "full",
         "early_stopping" : 20
     }
-    cup_best_params = grid_search_cv(cup_build_model, (cup_train_x, cup_train_y), cup_params, k_folds = 5, path="cup.csv")
+    cup_best_params = grid_search_cv(cup_build_model_train, (cup_train_x, cup_train_y), cup_params, k_folds = 5, path="cup.csv")
     cup_best_params_other, cup_best_params_training = split_train_params(cup_best_params, direct = False)
     print(cup_best_params_other, cup_best_params_training)
 
-    model = cup_build_model(**cup_best_params_other)
+    model = cup_build_model_test(**cup_best_params_other)
     history = model.training((cup_train_x, cup_train_y), (cup_test_x, cup_test_y), **cup_best_params_training, verbose = True)
 
     plt.plot(history["loss_tr"])
